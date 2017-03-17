28f72d35b162380e2af085f31443ce8643d78b6f
Matthias Fänger <matthias@faenger.de>
2017-03-17 14:54:26 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+// Include gulp
+const gulp = require('gulp');
+
+// Include Our Plugins
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const notify = require("gulp-notify");
+const imagemin = require('gulp-imagemin');
+const newer = require('gulp-newer');
+const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+
+// Image optimization
+gulp.task('images', function() {
+	return gulp.src('src/images/**/*')
+				.pipe(newer('images'))
+				.pipe(imagemin())
+				.pipe(notify("💥 Images minified!"))
+				.pipe(gulp.dest('images'));
+});
+
+// Compile and autoprefix Sass
+gulp.task('sass', function() {
+  return gulp.src('src/scss/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+				.pipe(autoprefixer('last 5 versions'))
+        .pipe(notify("💥 Sass compiled!"))
+        .pipe(gulp.dest('css'));
+});
+
+// Uglify & Babelify my JS
+gulp.task('scripts', function() {
+  return gulp.src('src/js/main.js')
+				.pipe(babel({
+						presets: ['es2015']
+				}))
+				.pipe(uglify())
+        .pipe(notify("💥 Scripts uglified and minified!"))
+        .pipe(gulp.dest('js'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('src/js/main.js', ['scripts']);
+    gulp.watch('src/scss/**/*.scss', ['sass']);
+		gulp.watch('src/images/**/*', ['images']);
+});
+
+// Default Task
+gulp.task('default', ['images', 'sass', 'scripts']);
---
+++ src/README.md
@@ -0,0 +1,21 @@
+# nofrills – clean Drupal 8 theme boilerplate
+nofrills is a clean Drupal 8 starter theme or boilerplate with Gulp, SASS (Autoprefixer), Babel (ES6), Uglify, Imagemin and more.
+
+This is really just a boilerplate so it'll look ugly! ;)
+
+### Features
+- Base theme is classy so you get nice classes in your HTML
+- Basic template files are included to be easily overwritten
+- Gulp to run and watch SASS (Autoprefixer), JS (Babel) and images (imagemin with newer)
+- Gulp notifier for nice success and error messages
+- normalize.css from classy theme
+
+### Requirements
+- Node
+- NPM or Yarn
+- Gulp
+
+### Installation
+- Install as you always do (UI or Drush or Drupal Console or Composer)
+- Go to your template folder and run `npm install` or `yarn install`
+- Go and start building your theme 😃
