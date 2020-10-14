cf5f52b088dd2ae09e963dfb54f74d422ca94840
Matthias Fänger <matthias.faenger@muuuh.de>
2020-10-14 09:51:40 +0200

    Update npm packages, especially to webpack 5

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -9,7 +9,10 @@ module.exports = {
   entry: './src/js/main.js',
   output: {
     path: path.resolve(__dirname, '../assets'),
-    filename: 'js/bundle.js'
+    filename: 'js/bundle.js',
+  },
+  stats: {
+    colors: true,
   },
   module: {
     rules: [
@@ -19,34 +22,32 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
           {
-            loader: MiniCssExtractPlugin.loader
+            loader: MiniCssExtractPlugin.loader,
           },
           {
-            loader: 'css-loader'
+            loader: 'css-loader',
           },
           {
             loader: 'postcss-loader',
             options: {
-              config: {
-                path: `${path.resolve(__dirname, '../postcss.config.js')}`
-              }
-            }
+              postcssOptions: { config: `${path.resolve(__dirname, '../postcss.config.js')}` },
+            },
           },
           {
             loader: 'sass-loader',
             options: {
-              implementation: require('sass')
-            }
-          }
-        ]
+              implementation: require('sass'),
+            },
+          },
+        ],
       },
       {
         test: /\.(woff|woff2|ttf|otf|eot)$/,
@@ -54,26 +55,27 @@ module.exports = {
           {
             loader: 'file-loader',
             options: {
-              outputPath: 'fonts'
-            }
-          }
-        ]
-      }
-    ]
+              outputPath: 'fonts',
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: 'css/bundle.css'
+      filename: 'css/bundle.css',
     }),
     new CopyPlugin({
       patterns: [
-      { context: 'src', from: 'images/**/*.+(jpg|jpeg|png|gif|svg)' },
-      { context: 'src', from: 'fonts/*.+(woff|woff2|otf|eot|svg)' }
-    ]}),
+        { context: 'src', from: 'images/**/*.+(jpg|jpeg|png|gif|svg)' },
+        { context: 'src', from: 'fonts/*.+(woff|woff2|otf|eot|svg)' },
+      ],
+    }),
     new ImageminPlugin({
       test: /\.(jpe?g|png|gif|svg)$/i,
-      plugins: [imageminMozjpeg({ quality: 80, progressive: true })]
-    })
-  ]
+      plugins: [imageminMozjpeg({ quality: 80, progressive: true })],
+    }),
+  ],
 }
