fd20977426cd6dc03b0808321a5a787bd9aefaed
Matthias Fänger <matthias.faenger@muuuh.de>
2020-10-14 10:04:07 +0200

    Add OptimizeCSSAssetsPlugin to webpack production config

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,6 +1,8 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  plugins: [new OptimizeCSSAssetsPlugin({})],
 })
